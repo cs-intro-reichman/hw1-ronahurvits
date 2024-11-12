878e47874a8588a12b1cf3ba074f50aa5e79b348
Rona Hurvits <ronahurvits@Ronas-MacBook-Pro.local>
2024-11-12 21:36:18 +0200

    exc1 after changes

--- .history/FVCalc_20241112213517.java
+++ .history/FVCalc_20241112213517.java
@@ -5,6 +5,6 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1])/100;
 		int currVal = Integer.parseInt(args[0]);
 		double futureVal = (currVal*(Math.pow((1+rate),years)));
-		System.out.println("After "+years+ " years, $"+currVal+" saved at "+rate*100+"%25 will yield $"+((int) futureVal));
+		System.out.println("After "+years+ " years, $"+currVal+" saved at "+rate+"% will yield $"+((int) futureVal));
 	} 
 }
